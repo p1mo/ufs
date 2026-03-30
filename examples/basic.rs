174b6a3eed1446585e4c276ca60f563738cea201
p1mo <101986259+p1mo@users.noreply.github.com>
2026-03-30 13:11:21 +0200

    fix version & example

--- examples/basic.rs
+++ examples/basic.rs
@@ -2,12 +2,9 @@ use ufs::{ bind_dir, UnifiedFS };
 
 
 
+fn main() -> anyhow::Result<()> {
 
-
-
-fn embed() -> anyhow::Result<()> {
-
-    let fs1 = bind_dir!("examples/files", read = false);
+    let fs1 = bind_dir!("examples/files");
 
     for item in fs1.iter() {
 
@@ -16,37 +13,14 @@ fn embed() -> anyhow::Result<()> {
         println!("embed > {:?}", item.path.exists());
     }
 
-    for item in fs1.walk(concat!(env!("CARGO_MANIFEST_DIR"), "/examples/files")) {
-
-        println!("local > {:?}", item);
-
-        println!("local > {:?}", item.path.exists());
-    }
-
-    Ok(())
-
-}
-
-fn local() -> anyhow::Result<()> {
-
     let fs2 = UnifiedFS::new();
 
     for item in fs2.walk(concat!(env!("CARGO_MANIFEST_DIR"), "/examples/files")) {
         println!("local > {:?}", item);
     }
 
-    Ok(())
-
-}
-
-
-
-fn main() -> anyhow::Result<()> {
-
-    embed()?;
-    //local()?;
-
     loop {
         std::thread::sleep(std::time::Duration::from_millis(1000));
     }
+
 } 
